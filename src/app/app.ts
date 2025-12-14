ee7c243f56af7564d908bb8cf361173a1b99def3
mubinsaeed <mubin@retrocausal.ai>
2025-12-14 14:22:46 +0500

    updated the navbar and other

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Homepage } from '../views/homepage/homepage';
 import { Taskpage } from '../views/taskpage/taskpage';
+import { Navbar } from '../components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Taskpage],
+  imports: [RouterOutlet, Taskpage, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
